4a7f3549cf2ff6125a9ecc595b7f965d20180f09
Szekely Csaba <csaba@szekely.ro>
2021-02-24 18:11:23 +0200

    Database connection setup done, and testetd

--- controller/db.php
+++ controller/db.php
@@ -6,7 +6,7 @@ class DB {
 
     public static function connectWriteDB(){
         if(self::$writeDBConnection === null){
-            self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=taskdb;charset=utf8', 'root', 'bubbnacs');
+            self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', 'root', 'bubbancs');
             self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
@@ -15,7 +15,7 @@ class DB {
 
     public static function connectReadDB(){
         if(self::$readDBConnection === null){
-            self::$readDBConnection = new PDO('mysql:host=localhost;dbname=taskdb;charset=utf8', 'root', 'bubbnacs');
+            self::$readDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', 'root', 'bubbancs');
             self::$readDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$readDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
