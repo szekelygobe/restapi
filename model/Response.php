d95726dd6523953ee7d3f54d31959918492a15fb
Szekely Csaba <csaba@szekely.ro>
2021-02-24 15:29:32 +0200

    Respones class done

--- model/Response.php
+++ model/Response.php
@@ -29,10 +29,30 @@ class Response {
     }
 
     public function send(){
+        header('Content-type application/json;charset=utf-8');
+
+        if($this-toCache == true){
+            header('Cache-control: max-age=60');
+        } else {
+            header('Cache-control: no-cache, no-store');
+        }
+
+        if(($this->_success !== false && $this->_success !== true) || !is_numeric($this->_httpStatusCode)){
+            http_response_code(500);
+            $this->_responseData['statusCode'] = 500;
+            $this->_responseData['success'] = false;
+
+            $this->addMessage('Response creation error');
+            $this->_responseData['messages'] = $this->_messages;
+        } else {
+            http_response_code($this->_httpStatusCode);
+            $this->_responseData['statusCode'] = $this->_httpStatusCode;
+            $this->_responseData['success'] = $this->_success;
+            $this->_responseData['messages'] = $this->_messages;
+            $this->_responseData['data'] = $this->_data;
+        }
         
-
-
-
+        echo json_decode($this->_responseData);
     }
 
 
