801b26a79b2b8d203b50b5092ff92017b24c46e7
Szekely Csaba <csaba@szekely.ro>
2021-03-02 16:12:58 +0200

    Added task list pagination and tested

--- controller/task.php
+++ controller/task.php
@@ -243,6 +243,119 @@ else if (array_key_exists('completed', $_GET)){
     }
 
 }
+// if tasks with pagination
+else if (array_key_exists('page', $_GET)) {
+    // pagination only on GET request method
+    if($_SERVER['REQUEST_METHOD'] === 'GET'){
+        $page = $_GET['page'];
+        $limitPerPage = 2;
+        // error handling ofr page number
+        if($page == '' || !is_numeric($page)){
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage('Page number cannot be blank and must be numeric - '.$page);
+            $response->send();
+            exit();
+        }
+
+        try {
+            // counting tasks in DB
+            $query = $readDB->prepare('SELECT count(id) as totalNoOfTasks from tbltasks');
+            $query->execute();
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+            $tasksCount = intval($row['totalNoOfTasks']);
+            // calculating number of pages we need (rounded up), minimum of 1 page
+            $numOfPages = $tasksCount > 0
+                ? ceil($tasksCount / $limitPerPage)
+                : 1;
+
+            // handling page errors
+            if($page > $numOfPages || $page == 0){
+                $response = new Response();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage('Page not found');
+                $response->send();
+                exit();
+            }
+
+            // calculating tasks to return
+            $offset = $page == 1 ? 0 : $limitPerPage*($page-1);
+
+            $query = $readDB->prepare('
+                SELECT 
+                    id, 
+                    title, 
+                    description, 
+                    DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, 
+                    completed
+                FROM tbltasks
+                LIMIT :p_pagelimit offset :p_offset
+            ');
+            $query->bindParam(':p_pagelimit', $limitPerPage, PDO::PARAM_INT);
+            $query->bindParam(':p_offset', $offset, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            $taskArray = [];
+
+            while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                $task = new Task(
+                    $row['id'],
+                    $row['title'],
+                    $row['description'],
+                    $row['deadline'],
+                    $row['completed']
+                );
+                $taskArray[] = $task->returnTaskAsArray();
+            }
+
+            $returnData = [];
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['total_rows'] = $tasksCount;
+            $returnData['total_pages'] = $numOfPages;
+            // returning pagination info
+            $page < $numOfPages ? $returnData['has_next_page'] = true : $returnData['has_next_page'] =  false;
+            $page > 1 ? $returnData['has_previous_page'] = true : $returnData['has_previous_page'] =  false;
+
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit();
+
+        } catch (TaskException $ex){
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($ex->getMessage());
+            $response->send();
+            exit();
+        } catch (PDOException $ex) {
+            error_log("Database query error - ".$ex, 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage('Failed to get tasks' );
+            $response->send();
+            exit();
+        } // try catch
+    }
+    // if not GET method
+    else {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage('Request method not allowed -'.$_SERVER['REQUEST_METHOD']);
+        $response->send();
+        exit();
+    } // end if not GET method
+}
 // if $_GET is empty for all tasks
 else if (empty($_GET)){
     // if requesting list of tasks
